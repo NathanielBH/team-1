32e79753d4d7b9efd9d3461553eb404bad532ba1
Alisa Hanada <123008646+alisahanada@users.noreply.github.com>
2023-10-24 10:44:04 -0400

    Update rgb_to_hex.cc

--- programming files/rgb_to_hex.cc
+++ programming files/rgb_to_hex.cc
@@ -1,3 +1,6 @@
+// Bug 1: The "g" value is not properly converted to hexadecimal & // Bug 2: Missing "g" conversion to hexadecimal 
+// a.h.
+
 #include <iostream>
 #include <sstream>
 #include <iomanip>
@@ -5,9 +8,8 @@
 // String function that converts RGB Code to Hexadecimal Code
 std::string rgb_to_hex(int r, int g, int b)
 {
-    // r, g, and b are integer variables that are the biggest number compared between 0 and the lowest number of 255 and the inputted value.
     r = std::max(0, std::min(255, r));
-    g = std::max(0, std::min(255, g));
+    // g = std::max(0, std::min(255, g));
     b = std::max(0, std::min(255, b));
 
     std::stringstream ss;
@@ -16,5 +18,6 @@ std::string rgb_to_hex(int r, int g, int b)
 
     return ss.str();
 }
+
 
 //Test with std::string hexColor = rgb_to_hex(255, 127, 0); // returns "FF7F00"
