283d5772fba36fb420fb9c8049a713ac698153c6
Nathan Seashore Hobson <46633219+NathanSeashoreHobson@users.noreply.github.com>
2023-10-24 10:49:11 -0400

    Add bugs to datetime_convert.py

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -1,11 +1,13 @@
+# Remove % formatting in string. Change the order of date object. : NSH
+
 from datetime import datetime
 
 # Set Date and Time in string
 date_str = "2022-03-17 10:45:30"
 # Read date_str into a datetime object
-date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+date_obj = datetime.strptime(date_str, '%Y-%d-%m %H:%M:%S')
 # Read the date object out into a formatted string M/D/Y H:M:S
-formatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')
+formatted_date = date_obj.strftime('m/d/Y H:M:S')
 
 # Print the formatted date string
 print(formatted_date)
