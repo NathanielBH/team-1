f43707e6a91b6f1ac9d9afa3e639de97757c2e15
Alisa Hanada <123008646+alisahanada@users.noreply.github.com>
2023-10-24 10:48:02 -0400

    Update datetime_convert.java

--- programming files/datetime_convert.java
+++ programming files/datetime_convert.java
@@ -1,20 +1,26 @@
+// Bug 1: Change the dateStr format
+// Original dateStr: "2022-03-17 10:45:30"
+// Bug 2: Incorrect format pattern
+// Use "dd/MM/yyyy HH:mm:ss" instead of "MM/dd/yyyy HH:mm:ss"
+// a.h.
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-
 public class datetime_convert {
-    
-    public static void main(String[] args) {
-        //field dateStr
-        String dateStr = "2022-03-17 10:45:30";
 
-        //create format for date strings
+    public static void main(String[] args) {
+        String dateStr = "17-03-2022 10:45:30";
+
+        // Create format for date strings
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        //create dateObj using dateStr and formatter
+        // Create dateObj using dateStr and formatter
         LocalDateTime dateObj = LocalDateTime.parse(dateStr, formatter);
-        //convert object to string forMattedDate
-        String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"));
-        //print the formatted string
+
+        // Convert object to string formattedDate
+        String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+
+        // Print the formatted string
         System.out.println(formattedDate);
     }
 }
